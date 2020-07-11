10e409d0154bc0694d82d81f7acd62b0c056331d
ironsoul0 <timka2609@gmail.com>
2020-07-11 14:06:41 +0600

    more tests

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -2,8 +2,25 @@ pragma solidity ^0.5.0;
  
 contract SocialNetwork {
   string public name;
+  uint public postCount = 0;
+  mapping (uint => Post) public posts; 
+
+  struct Post {
+    uint id;
+    string content;
+    uint tipAmount;
+    address author;
+  }
+
+  event PostCreated(uint id, string content, uint tipAmount, address author);
   
   constructor() public {
     name = "Social network";
+  }
+
+  function createPost(string memory _content) public {
+    postCount++;
+    posts[postCount] = Post(postCount, _content, 0, msg.sender);
+    emit PostCreated(postCount, _content, 0, msg.sender);
   }
 }
--- test/SocialNetwork.js
+++ test/SocialNetwork.js
@@ -5,7 +5,7 @@ const SocialNetwork = artifacts.require("SocialNetwork");
 
 require('chai').use(require('chai-as-promised')).should()
 
-contract('SocialNetwork', (accounts) => {
+contract('SocialNetwork', ([deployer, author, tipper]) => {
   let socialNetwork 
 
   before(async () => {
@@ -24,6 +24,24 @@ contract('SocialNetwork', (accounts) => {
     it('has a name', async () => {
       const name = await socialNetwork.name()
       assert.equal(name, 'Social network')
+    })
+  })
+
+  describe('posts', async () => {
+    it('create posts', async () => {
+      const result = await socialNetwork.createPost("This is my first post", { from: author })
+      const postCount = await socialNetwork.postCount();
+      assert.equal(postCount, 1)
+      const event = result.logs[0].args
+      console.log(event)
+    })
+
+    it('lists posts', async () => {
+
+    })
+
+    it('allow users to tip posts', async () => {
+
     })
   })
 })
