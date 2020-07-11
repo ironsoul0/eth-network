8f681e6917cc21a141d9a2b60a4a752c302657e4
ironsoul0 <timka2609@gmail.com>
2020-07-11 15:23:01 +0600

    add web3 load

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
 
 class App extends Component {
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvided);
+    } else {
+      window.alert("No Ethereum browser");
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts)
+  }
+
   render() {
     return (
       <div>
